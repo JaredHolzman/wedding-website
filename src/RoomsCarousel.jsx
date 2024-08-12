9d8a39ab6a9105ba983cd9d8e1ccb126666337ca
Jared Holzman <jared@acadiaclimate.com>
2024-08-11 22:03:43 -0700

    Add room carousel content.

--- src/RoomsCarousel.jsx
+++ src/RoomsCarousel.jsx
@@ -20,11 +20,9 @@ function RoomSlide({
   return (
     <div id={id} className="carousel-item grid w-full">
       <div>
-        <hgroup className="mx-6 mb-6">
-          <h3 className="mb-4 text-xl">{roomType}</h3>
-          <p>{description}</p>
-        </hgroup>
+        <h3 className="mb-6 text-xl">{roomType}</h3>
         <img src={imgSrc} className="w-full" />
+        <p className="mx-4 mt-6">{description}</p>
         <div className="mt-4 overflow-x-auto">
           <table className="table">
             <thead>
@@ -56,16 +54,16 @@ export default function RoomsCarousel() {
   return (
     <div>
       <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#1b-suite" className="btn btn-sm">
+        <a href="#room-1b" className="btn btn-sm">
           Standard
         </a>
-        <a href="#item2" className="btn btn-sm">
+        <a href="#room-sky-view" className="btn btn-sm">
           Sky View
         </a>
-        <a href="#item3" className="btn btn-sm">
+        <a href="#room-swim-up" className="btn btn-sm">
           Swim Up
         </a>
-        <a href="#item4" className="btn btn-sm">
+        <a href="#room-ocean-view" className="btn btn-sm">
           Ocean View
         </a>
       </div>
@@ -74,25 +72,54 @@ export default function RoomsCarousel() {
       </div>
       <div className="carousel my-6 w-full">
         <RoomSlide
-          id="1b-suite"
+          id="room-1b"
           imgSrc={room1B}
           roomType="One Bedroom Suite"
           description={`
-            Pool view. Located on 2nd, 3rd or 4th floor. Bathtub. Outdoor
-            jacuzzi. Bedding 1 king or 2 queens. 1000 sq ft.
+            Pool view, located on the second and third floors, with a bathtub
+            and outdoor jacuzzi, living room and bedroom area separated by
+            doors. Bedding 1 king or 2 queen size. 1000 sq ft.
           `}
           familyRates={["$612.00", "$805.00", "$999.00"]}
           clubRates={["$635.00", "$836.00", "$1038.00"]}
         />
-        <div id="item2" className="carousel-item w-full">
-          <img src={roomSkyView} className="w-full" />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <img src={roomSwimUp} className="w-full" />
-        </div>
-        <div id="item4" className="carousel-item w-full">
-          <img src={roomOceanView} className="w-full" />
-        </div>
+
+        <RoomSlide
+          id="room-sky-view"
+          imgSrc={roomSkyView}
+          roomType="Sky View Suite"
+          description={`
+            Pool and Sky view, located on 4th floor, room divided in two floors
+            (Main room plus a large roof top terrace with an outdoor Jacuzzi).
+            Bedding 1 king. 1000 sq ft divided room and terrace.
+          `}
+          familyRates={["$612.00", "$805.00", "$999.00"]}
+          clubRates={["$635.00", "$836.00", "$1038.00"]}
+        />
+        <RoomSlide
+          id="room-swim-up"
+          imgSrc={roomSwimUp}
+          roomType="Swim Up Suite"
+          description={`
+            Pool view, located on ground floor, with a bathtub and outdoor
+            Jacuzzi, direct access to the pool and a small garden including
+            a Bali Bed. Bedding 1 king or 2 queens. 1000 sq ft.
+          `}
+          familyRates={["$612.00", "$805.00", "$999.00"]}
+          clubRates={["$635.00", "$836.00", "$1038.00"]}
+        />
+        <RoomSlide
+          id="room-ocean-view"
+          imgSrc={roomOceanView}
+          roomType="Ocean View Suite"
+          description={`
+            Ocean view. Located 2nd, 3rd, or 4th floor. Bathtub. Outdoor
+            Jacuzzi. Living room. Bedroom area separated by doors. Bedding 1
+            king or 2 queen. 1000 sq ft.
+          `}
+          familyRates={["$612.00", "$805.00", "$999.00"]}
+          clubRates={["$635.00", "$836.00", "$1038.00"]}
+        />
       </div>
       <p className="text-sm">
         {asterick}These are estimates and subject to change until you book.

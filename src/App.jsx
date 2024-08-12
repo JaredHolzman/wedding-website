394fff293a4d65798024e411f1d17dc63e00d7b0
Jared Holzman <jared@acadiaclimate.com>
2024-08-11 17:27:08 -0700

    Add accomodations section.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <main className="w-full">
       <Section
-        heading={"We're getting married!"}
+        heading={"We're Getting Married!"}
         subheading={
           'Come join us for a beach-side destination wedding as we say "I do" under the beautiful Punta Cana sun'
         }
@@ -20,6 +20,34 @@ function App() {
           <button className="btn">More Info</button>
         </div>
       </Section>
+      <section className="grid w-full">
+        <img src={wideSideBySide} />
+        <div className="p-8 text-center">
+          <h2 className="mb-4 text-2xl">Accommodations</h2>
+          <p>
+            The ceremony will be held at Majest Mirage Punta Cana. We are
+            working with Honeymoons, Inc. to handle room reservations. All
+            bookings will include shuttle transfer to and from the airport. To
+            allow for flexibility, rooms are NOT reserved. Please book as early
+            as possible to lock-in the best possible rate for yourself. See
+            below for room options and booking details.
+          </p>
+        </div>
+      </section>
+      <section className="grid w-full">
+        <img src={wideSideBySide} />
+        <div className="p-8 text-center">
+          <h2 className="mb-4 text-2xl">Accommodations</h2>
+          <p>
+            The ceremony will be held at Majest Mirage Punta Cana. We are
+            working with Honeymoons, Inc. to handle room reservations. All
+            bookings will include shuttle transfer to and from the airport. To
+            allow for flexibility, rooms are NOT reserved. Please book as early
+            as possible to lock-in the best possible rate for yourself. See
+            below for room options and booking details.
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
--- src/Section.jsx
+++ src/Section.jsx
@@ -3,7 +3,7 @@ export default function Section({ heading, subheading, imgSrc, children }) {
     <section className="grid w-full">
       <img src={imgSrc} />
       <div className="p-8 text-center">
-        <h2 className="text-2xl mb-4">{heading}</h2>
+        <h2 className="mb-4 text-2xl">{heading}</h2>
         <p>{subheading}</p>
         {children}
       </div>

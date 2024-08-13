b2a794c0e45877e371fd373358f2ea4a808c72a5
Jared Holzman <jared@acadiaclimate.com>
2024-08-12 21:26:44 -0700

    Add footer.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,33 @@ import LandingSection from "./LandingSection";
 import LocationSection from "./LocationSection";
 import FAQSection from "./FAQSection";
 
+import heart from "./assets/heart.svg";
+
 function App() {
   return (
-    <main className="w-full text-center">
-      <LandingSection />
-      <LocationSection />
-      <BookingSection />
-      <RoomsSection />
-      <FAQSection />
-    </main>
+    <>
+      <main className="w-full text-center">
+        <LandingSection />
+        <LocationSection />
+        <BookingSection />
+        <RoomsSection />
+        <FAQSection />
+      </main>
+      <footer className="footer footer-center mt-8 bg-base-300 p-4 text-base-content">
+        <aside>
+          <p>
+            made with
+            <img
+              src={heart}
+              className="mx-1.5 inline h-4"
+              alt="love"
+              title="love"
+            />
+            by Jared & Joanna
+          </p>
+        </aside>
+      </footer>
+    </>
   );
 }
 
